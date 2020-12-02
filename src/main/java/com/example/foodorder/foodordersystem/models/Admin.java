f143584a9a00498fe97ac2ee005f4df7ee34da72
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 16:15:50 +0000

    working on admin class

--- src/main/java/com/example/foodorder/foodordersystem/models/Admin.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Admin.java
@@ -1,4 +1,24 @@
 package com.example.foodorder.foodordersystem.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "admins")
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "admin_name")
+    private String adminName;
+
+    @JsonIgnoreProperties(value="admins")
+    @OneToMany(mappedBy = "admin", fetch = FetchType.LAZY)
+    private List<Product> products;
+
 }
