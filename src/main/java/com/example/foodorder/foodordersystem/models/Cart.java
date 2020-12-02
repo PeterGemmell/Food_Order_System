e3fc84e0018b1efc34758bbecbd11a56aa45788a
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 17:49:43 +0000

    cart class laid out

--- src/main/java/com/example/foodorder/foodordersystem/models/Cart.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Cart.java
@@ -1,4 +1,65 @@
 package com.example.foodorder.foodordersystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="carts")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="cart_quantity")
+    private Integer cartQuantity;
+
+    @Column(name="cart_total")
+    private Double cartTotal;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+
+    public Cart(Integer cartQuantity, Double cartTotal, Customer customer){
+        this.cartQuantity = cartQuantity;
+        this.cartTotal = cartTotal;
+        this.customer = customer;
+    }
+
+    public Cart(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCartQuantity() {
+        return cartQuantity;
+    }
+
+    public void setCartQuantity(Integer cartQuantity) {
+        this.cartQuantity = cartQuantity;
+    }
+
+    public Double getCartTotal() {
+        return cartTotal;
+    }
+
+    public void setCartTotal(Double cartTotal) {
+        this.cartTotal = cartTotal;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
