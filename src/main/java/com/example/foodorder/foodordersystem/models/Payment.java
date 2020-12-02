c5cc0b7c275231755538e226d8ec3b9e688c11e2
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 17:39:55 +0000

    payment class laid out

--- src/main/java/com/example/foodorder/foodordersystem/models/Payment.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Payment.java
@@ -1,4 +1,77 @@
 package com.example.foodorder.foodordersystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="payments")
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="customer_ref")
+    private String customerRef;
+
+    @Column(name="card_type")
+    private String cardType;
+
+    @Column(name="card_number")
+    private String cardNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Payment(String customerRef, String cardType, String cardNumber, Customer customer){
+        this.customerRef = customerRef;
+        this.cardType = cardType;
+        this.cardNumber = cardNumber;
+        this.customer = customer;
+
+    }
+
+    public Payment(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerRef() {
+        return customerRef;
+    }
+
+    public void setCustomerRef(String customerRef) {
+        this.customerRef = customerRef;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
