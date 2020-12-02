86d8213be38cea81706942221450fe270926e360
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 17:30:31 +0000

    customer class laid out

--- src/main/java/com/example/foodorder/foodordersystem/models/Customer.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Customer.java
@@ -1,6 +1,9 @@
 package com.example.foodorder.foodordersystem.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="customers")
@@ -10,5 +13,91 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="customer_name")
+    private String customerName;
 
+    @Column(name="customer_address")
+    private String customerAddress;
+
+    @Column(name="phone_number")
+    private Integer phoneNumber;
+
+    @JsonIgnoreProperties(value = "customers")
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Product> products;
+
+    @OneToOne(mappedBy = "customers")
+    private Cart cart;
+
+    @OneToOne(mappedBy = "customers")
+    private Payment payment;
+
+    public Customer(String customerName, String customerAddress, Integer phoneNumber, Cart cart, Payment payment){
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.phoneNumber = phoneNumber;
+        this.cart = cart;
+        this.payment = payment;
+
+    }
+
+    public Customer(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
