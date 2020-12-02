6408a23e732f27fcec4e572ccd78c5e2700c948d
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 15:52:41 +0000

    models and repos laid out

---
+++ src/main/java/com/example/foodorder/foodordersystem/models/Product.java
@@ -0,0 +1,4 @@
+package com.example.foodorder.foodordersystem.models;
+
+public class Product {
+}
---
+++ src/main/java/com/example/foodorder/foodordersystem/repository/PaymentRepository.java
@@ -0,0 +1,4 @@
+package com.example.foodorder.foodordersystem.repository;
+
+public interface PaymentRepository {
+}
