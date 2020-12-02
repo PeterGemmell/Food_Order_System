7687c12feb44121627a29eff7d0523f588b55c5f
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-02 16:42:26 +0000

    product class laid out

--- src/main/java/com/example/foodorder/foodordersystem/models/Customer.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Customer.java
@@ -1,4 +1,14 @@
 package com.example.foodorder.foodordersystem.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
--- src/main/java/com/example/foodorder/foodordersystem/models/Product.java
+++ src/main/java/com/example/foodorder/foodordersystem/models/Product.java
@@ -1,4 +1,108 @@
 package com.example.foodorder.foodordersystem.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="product_name")
+    private String productName;
+
+    @Column(name="product_group")
+    private String productGroup;
+
+    @Column(name="sub_group")
+    private String subGroup;
+
+    @JsonIgnoreProperties(value="products")
+    @ManyToOne
+    @JoinColumn(name = "admin_id", nullable = false)
+    private Admin admin;
+
+    @JsonIgnoreProperties(value="products")
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @JsonIgnoreProperties(value="products")
+    @ManyToOne
+    @JoinColumn(name = "guest_id", nullable = false)
+    private Guest guest;
+
+    public Product(String productName, String productGroup, String subGroup, Admin admin, Customer customer, Guest guest){
+        this.productName = productName;
+        this.productGroup = productGroup;
+        this.subGroup = subGroup;
+        this.admin = admin;
+        this.customer = customer;
+        this.guest = guest;
+
+    }
+
+    public Product(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductGroup() {
+        return productGroup;
+    }
+
+    public void setProductGroup(String productGroup) {
+        this.productGroup = productGroup;
+    }
+
+    public String getSubGroup() {
+        return subGroup;
+    }
+
+    public void setSubGroup(String subGroup) {
+        this.subGroup = subGroup;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
 }
