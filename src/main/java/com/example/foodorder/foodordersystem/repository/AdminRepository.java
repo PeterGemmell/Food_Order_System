133acc0d42b6cf10f04d212fdb9e41d79608302d
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-03 10:32:39 +0000

    Admin repo created

--- src/main/java/com/example/foodorder/foodordersystem/repository/AdminRepository.java
+++ src/main/java/com/example/foodorder/foodordersystem/repository/AdminRepository.java
@@ -1,4 +1,9 @@
 package com.example.foodorder.foodordersystem.repository;
 
-public interface AdminRepository {
+import com.example.foodorder.foodordersystem.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Admin findByAdminName(String adminName);
 }
--- src/main/java/com/example/foodorder/foodordersystem/repository/CustomerRepository.java
+++ src/main/java/com/example/foodorder/foodordersystem/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Customer findByUserNameAndPassword(String userName, String password);
+    Customer findByCustomerName(String customerName);
 }
