c200a51181438519dda9132c65691ac9d8dbc741
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-03 10:28:03 +0000

    customer repo created

--- src/main/java/com/example/foodorder/foodordersystem/repository/CustomerRepository.java
+++ src/main/java/com/example/foodorder/foodordersystem/repository/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.example.foodorder.foodordersystem.repository;
 
-public interface CustomerRepository {
+import com.example.foodorder.foodordersystem.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByUserNameAndPassword(String userName, String password);
 }
